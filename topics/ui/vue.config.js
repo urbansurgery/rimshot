5708e71923ecb6d70247521db9299088553b90e2
Jonathon Broughton <jonathon@stardotbmp.com>
2022-05-13 12:51:23 +0100

    fix (ui): get this damn thing working despite depoendency juggling

--- topics/ui/vue.config.js
+++ topics/ui/vue.config.js
@@ -1,4 +1,6 @@
 const webpack = require('webpack');
+const path = require('path');
+// webpack configuration file
 
 const globalSassFiles = ['~@/theme/_variables.scss'];
 
@@ -9,53 +11,76 @@ module.exports = {
     port: 8080,
   },
   publicPath: '/',
-  chainWebpack: (config) => {
-    config.plugin('html').tap((args) => {
-      args[0].title = 'Rimstop | Realtime Issue Management with Speckle';
-      return args;
-    });
-    config.devtool('inline-source-map');
-    ['vue-modules', 'vue', 'normal-modules', 'normal'].forEach((match) => {
-      config.module
-        .rule('sass')
-        .oneOf(match)
-        .use('sass-loader')
-        .tap((opt) => {
-          return Object.assign(opt, {
-            additionalData: globalSassFiles
-              .map((src) => '@import "' + src + '"')
-              .join('\n'),
-          });
-        });
-    });
-  },
   configureWebpack: (config) => {
+    if (process.env.NODE_ENV === 'development') {
+      config.devtool = 'source-map';
+
+      config.output.devtoolModuleFilenameTemplate = (info) =>
+        info.resourcePath.match(/^\.\/\S*?\.vue$/)
+          ? `webpack-generated:///${info.resourcePath}?${info.hash}`
+          : `webpack-yourCode:///${info.resourcePath}`;
+
+      config.output.devtoolFallbackModuleFilenameTemplate =
+        'webpack:///[resource-path]?[hash]';
+    }
+
     config.plugins = [
       ...config.plugins, // this is important !
-      // new webpack.DefinePlugin({
-      //   'process.env': { PACKAGE_VERSION: `"${version}"` },
-      // }),
-      new webpack.LoaderOptionsPlugin({
-        debug: true,
-      }),
+      new webpack.DefinePlugin({}),
     ];
-    config.optimization = {
-      ...config.optimization,
-      runtimeChunk: 'single',
-      splitChunks: {
-        minSize: 10000,
-        maxSize: 250000,
-      },
-    };
-    config.performance = {
-      ...config.performance,
-      hints: false,
-    };
+
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'sass-loader',
+        {
+          loader: 'sass-resources-loader',
+          options: {
+            resources: [
+              path.resolve(__dirname, './src/theme/_variables.scss'), // global variables
+            ],
+          },
+        },
+      ],
+    });
+  },
+  chainWebpack: (config) => {
+    config.plugin('html').tap((args) => {
+      args[0].title = `Rimshot - Realtime Issue Management with Speckle`;
+      return args;
+    });
+  },
+  pwa: {
+    short_name: `Rimshot`,
+    name: `Rimshot - Realtime Issue Management with Speckle`,
+    themeColor: '#ffffff',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#7f0000',
+    display: 'minimal-ui',
+    scope: '/',
+
+    shortcuts: [],
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      //   swSrc: "dev/sw.js",
+      // ...other Workbox options...
+    },
   },
   css: {
     sourceMap: true,
     loaderOptions: {
       sass: {
+        // Here we can specify the older indent syntax formatted SASS
+        // Note that there is *not* a semicolon at the end of the below line
+        additionalData: globalSassFiles
+          .map((src) => '@import "' + src + '"')
+          .join('\n'),
+      },
+      scss: {
         additionalData: globalSassFiles
           .map((src) => '@import "' + src + '";')
           .join('\n'),
