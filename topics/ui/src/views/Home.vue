fcb65bfc832be112af2fd4a40bc6a5605b3bd60e
Jonathon Broughton <jonathon@stardotbmp.com>
2022-05-13 13:01:02 +0100

    feat (ui): the wrapper application, views and scaffold.
    I've made this more complicated than I should have.

--- topics/ui/src/views/Home.vue
+++ topics/ui/src/views/Home.vue
@@ -1,15 +1,11 @@
 <script>
   // @ is an alias to /src
-  // import HelloWorld from '@/components/HelloWorld.vue'
 
   export default {
     name: 'HomeView',
-    components: {
-      // HelloWorld,
-    },
   };
 </script>
 
 <template>
-  <div class="home" />
+  <div class="home"><h1>Rimshot</h1></div>
 </template>
