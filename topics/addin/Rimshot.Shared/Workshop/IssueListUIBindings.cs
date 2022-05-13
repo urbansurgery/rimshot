4f74e04f9e27f5a58928cb02a30ed4c64a4bda9d
jsdbroughton <jonathon@stardotbmp.com>
2022-05-13 18:06:18 +0100

    wip (speckle): Interop added.

--- topics/addin/Rimshot.Shared/Workshop/IssueListUIBindings.cs
+++ topics/addin/Rimshot.Shared/Workshop/IssueListUIBindings.cs
@@ -14,12 +14,18 @@ using Autodesk.Navisworks.Api;
 using ComApi = Autodesk.Navisworks.Api.Interop.ComApi;
 using ComBridge = Autodesk.Navisworks.Api.ComApi.ComApiBridge; //
 
+using Speckle.Core.Models;
+using Speckle.Core.Credentials;
+using Speckle.Core.Transports;
+using Speckle.Core.Api;
+using System.Reflection;
+
 namespace Rimshot.Shared.Workshop {
   public abstract class UIBindings {
 #if DEBUG
-    const string rimshotUrl = "http://192.168.86.42:8080/issues";
+    const string rimshotUrl = "https://rimshot.app/issues";
 #else
-    const String rimshotUrl = "https://rimshot.app/issues";;Boolean debug = false;
+    const String rimshotUrl = "https://rimshot.app/issues";
 #endif
     public const string Url = rimshotUrl;
 
@@ -65,7 +71,96 @@ namespace Rimshot.Shared.Workshop {
       return camera;
     }
 
-    private void CommitSelectedObjectsToSpeckle ( object payload ) => throw new NotImplementedException();
+    private async void CommitSelectedObjectsToSpeckle ( object payload ) {
+
+      Type payloadType = payload.GetType();
+
+      PropertyInfo streamProp = payloadType.GetProperty( "stream" );
+      PropertyInfo hostProp = payloadType.GetProperty( "host" );
+      PropertyInfo branchProp = payloadType.GetProperty( "branch" );
+      PropertyInfo issueProp = payloadType.GetProperty( "issueId" );
+
+      var commitPayload = ( dynamic )payload;
+
+      string branchName = commitPayload.branch;
+      string streamId = commitPayload.stream;
+      string host = commitPayload.host;
+      string issueId = commitPayload.issueId;
+
+
+      //{ "branch":"issues/xyz-2","host":"https://speckle.xyz","issueId":"au6SugFWUtOtv601Gekj","stream":"fd1d44405d"}
+
+      //Objects.Geometry.Mesh mesh = new Objects.Geometry.Mesh();
+
+      Objects.Geometry.Plane basePlane = new Objects.Geometry.Plane();
+      Objects.Primitive.Interval xSize = new Objects.Primitive.Interval( 1.0, 2.0 );
+      Objects.Primitive.Interval ySize = new Objects.Primitive.Interval( 1.0, 2.0 );
+      Objects.Primitive.Interval zSize = new Objects.Primitive.Interval( 1.0, 2.0 );
+      Objects.Geometry.Box box = new Objects.Geometry.Box( basePlane, xSize, ySize, zSize );
+
+      List<Objects.Geometry.Box> displayValue = new List<Objects.Geometry.Box>();
+
+      displayValue.Add( box );
+
+      Base myCommit = new Base();
+
+      myCommit[ "displayValue" ] = displayValue;
+
+      string description = $"issueId:{issueId}";
+
+      Account defaultAccount = AccountManager.GetDefaultAccount();
+
+      Client client = new Client( defaultAccount );
+
+      Branch branch;
+      try {
+        branch = await client.BranchGet( streamId, branchName );
+        if ( branch is null ) {
+          branch = await CreateBranch( client, streamId, branchName, description );
+        }
+      } catch ( Exception e ) {
+        Console.WriteLine( $"Error: {e.Message}" );
+      }
+
+      //var branch = client.BranchGet( streamId, branchName, 1 ).Result;
+
+
+
+
+    }
+
+    public Task<Branch> CreateBranch ( Client client, string streamId, string branchName, string description ) {
+
+      try {
+        var branchId = client.BranchCreate( new BranchCreateInput() {
+          streamId = streamId,
+          name = branchName,
+          description = description
+        } );
+      } catch ( Exception e ) {
+        Console.WriteLine( $"Error: {e.Message}" );
+      }
+
+      var branch = client.BranchGet( streamId, branchName );
+
+      Console.WriteLine( value: $"Branch: {branch}" );
+
+      return branch;
+      //try {
+      //  var branchId = await Operations.StreamState.Client.BranchCreate( new BranchCreateInput() {
+      //    streamId = StreamState.Stream.id,
+      //    name = BranchName,
+      //    description = BranchDescription
+      //  } );
+      //} catch ( Exception e ) {
+      //  Notifications.Enqueue( $"Error: {e.Message}" );
+      //  return;
+      //}
+
+      //var branch = await StreamState.Client.BranchGet( StreamState.Stream.id, BranchName );
+
+      //DialogHost.CloseDialogCommand.Execute( branch, null );
+    }
 
     private void SendIssueView () {
 
