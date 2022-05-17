72c08ab8f6a394893178b1e06002c1399adfb010
jsdbroughton <jonathon@stardotbmp.com>
2022-05-17 17:43:40 +0100

    fix (geometry): FIXED GEOMETRY!!

--- topics/addin/Rimshot.Shared/Geometry/Geometry.cs
+++ topics/addin/Rimshot.Shared/Geometry/Geometry.cs
@@ -6,70 +6,82 @@ using ComBridge = Autodesk.Navisworks.Api.ComApi.ComApiBridge; //
 
 namespace Rimshot.Shared {
   public class CallbackGeomListener : ComApi.InwSimplePrimitivesCB {
-    public List<float> Coords { get; set; }
+    public List<double> Coords { get; set; }
+
+    public List<int> Faces { get; set; }
+    public List<NavisTriangle> Triangles { get; set; }
     public float[] Matrix { get; set; }
     public CallbackGeomListener () {
-      this.Coords = new List<float>();
+      this.Coords = new List<double>();
+      this.Faces = new List<int>();
+      this.Triangles = new List<NavisTriangle>();
     }
     public void Line ( ComApi.InwSimpleVertex v1, ComApi.InwSimpleVertex v2 ) { }
     public void Point ( ComApi.InwSimpleVertex v1 ) { }
     public void SnapPoint ( ComApi.InwSimpleVertex v1 ) { }
-
     public void Triangle ( ComApi.InwSimpleVertex v1, ComApi.InwSimpleVertex v2, ComApi.InwSimpleVertex v3 ) {
 
-
+      int indexPointer = this.Faces.Count;
 
       Array array_v1 = ( Array )v1.coord;
-      float v1_x = ( float )array_v1.GetValue( 1 );
-      float v1_y = ( float )array_v1.GetValue( 2 );
-      float v1_z = ( float )array_v1.GetValue( 3 );
+      float v1X = ( float )array_v1.GetValue( 1 );
+      float v1Y = ( float )array_v1.GetValue( 2 );
+      float v1Z = ( float )array_v1.GetValue( 3 );
 
       Array array_v2 = ( Array )v2.coord;
-      float v2_x = ( float )array_v2.GetValue( 1 );
-      float v2_y = ( float )array_v2.GetValue( 2 );
-      float v2_z = ( float )array_v2.GetValue( 3 );
+      float v2X = ( float )array_v2.GetValue( 1 );
+      float v2Y = ( float )array_v2.GetValue( 2 );
+      float v2Z = ( float )array_v2.GetValue( 3 );
 
       Array array_v3 = ( Array )v3.coord;
-      float v3_x = ( float )array_v3.GetValue( 1 );
-      float v3_y = ( float )array_v3.GetValue( 2 );
-      float v3_z = ( float )array_v3.GetValue( 3 );
+      float v3X = ( float )array_v3.GetValue( 1 );
+      float v3Y = ( float )array_v3.GetValue( 2 );
+      float v3Z = ( float )array_v3.GetValue( 3 );
 
-      //Matrix
+      //Matrix transformation
+      float T1 = Matrix[ 3 ] * v1X + Matrix[ 7 ] * v1Y + Matrix[ 11 ] * v1Z + Matrix[ 15 ];
+      float v1X_ = ( Matrix[ 0 ] * v1X + Matrix[ 4 ] * v1Y + Matrix[ 8 ] * v1Z + Matrix[ 12 ] ) / T1;
+      float v1Y_ = ( Matrix[ 1 ] * v1X + Matrix[ 5 ] * v1Y + Matrix[ 9 ] * v1Z + Matrix[ 13 ] ) / T1;
+      float v1Z_ = ( Matrix[ 2 ] * v1X + Matrix[ 6 ] * v1Y + Matrix[ 10 ] * v1Z + Matrix[ 14 ] ) / T1;
 
-      float w1 = Matrix[ 3 ] * v1_x + Matrix[ 7 ] * v1_y + Matrix[ 11 ] * v1_z + Matrix[ 15 ];
+      float T2 = Matrix[ 3 ] * v2X + Matrix[ 7 ] * v2Y + Matrix[ 11 ] * v2Z + Matrix[ 15 ];
+      float v2X_ = ( Matrix[ 0 ] * v2X + Matrix[ 4 ] * v2Y + Matrix[ 8 ] * v2Z + Matrix[ 12 ] ) / T2;
+      float v2Y_ = ( Matrix[ 1 ] * v2X + Matrix[ 5 ] * v2Y + Matrix[ 9 ] * v2Z + Matrix[ 13 ] ) / T2;
+      float v2Z_ = ( Matrix[ 2 ] * v2X + Matrix[ 6 ] * v2Y + Matrix[ 10 ] * v2Z + Matrix[ 14 ] ) / T2;
 
-      float v1__x = ( Matrix[ 0 ] * v1_x + Matrix[ 4 ] * v1_y + Matrix[ 8 ] * v1_z + Matrix[ 12 ] ) / w1;
-      float v1__y = ( Matrix[ 1 ] * v1_x + Matrix[ 5 ] * v1_y + Matrix[ 9 ] * v1_z + Matrix[ 13 ] ) / w1;
-      float v1__z = ( Matrix[ 2 ] * v1_x + Matrix[ 6 ] * v1_y + Matrix[ 10 ] * v1_z + Matrix[ 14 ] ) / w1;
-
-
-      float w2 = Matrix[ 3 ] * v2_x + Matrix[ 7 ] * v2_y + Matrix[ 11 ] * v2_z + Matrix[ 15 ];
-
-      float v2__x = ( Matrix[ 0 ] * v2_x + Matrix[ 4 ] * v2_y + Matrix[ 8 ] * v2_z + Matrix[ 12 ] ) / w2;
-      float v2__y = ( Matrix[ 1 ] * v2_x + Matrix[ 5 ] * v2_y + Matrix[ 9 ] * v2_z + Matrix[ 13 ] ) / w2;
-      float v2__z = ( Matrix[ 2 ] * v2_x + Matrix[ 6 ] * v2_y + Matrix[ 10 ] * v2_z + Matrix[ 14 ] ) / w2;
-
-      float w3 = Matrix[ 3 ] * v3_x + Matrix[ 7 ] * v3_y + Matrix[ 11 ] * v3_z + Matrix[ 15 ];
-
-      float v3__x = ( Matrix[ 0 ] * v3_x + Matrix[ 4 ] * v3_y + Matrix[ 8 ] * v3_z + Matrix[ 12 ] ) / w3;
-      float v3__y = ( Matrix[ 1 ] * v3_x + Matrix[ 5 ] * v3_y + Matrix[ 9 ] * v3_z + Matrix[ 13 ] ) / w3;
-      float v3__z = ( Matrix[ 2 ] * v3_x + Matrix[ 6 ] * v3_y + Matrix[ 10 ] * v3_z + Matrix[ 14 ] ) / w3;
+      float T3 = Matrix[ 3 ] * v3X + Matrix[ 7 ] * v3Y + Matrix[ 11 ] * v3Z + Matrix[ 15 ];
+      float v3X_ = ( Matrix[ 0 ] * v3X + Matrix[ 4 ] * v3Y + Matrix[ 8 ] * v3Z + Matrix[ 12 ] ) / T3;
+      float v3Y_ = ( Matrix[ 1 ] * v3X + Matrix[ 5 ] * v3Y + Matrix[ 9 ] * v3Z + Matrix[ 13 ] ) / T3;
+      float v3Z_ = ( Matrix[ 2 ] * v3X + Matrix[ 6 ] * v3Y + Matrix[ 10 ] * v3Z + Matrix[ 14 ] ) / T3;
 
 
+      this.Faces.Add( 0 ); //TRIANGLE FLAG
 
-      Coords.Add( ( float )v1__x );
-      Coords.Add( ( float )v1__y );
-      Coords.Add( ( float )v1__z );
+      // Triangle by 3 Vertices
+      this.Coords.Add( v1X_ );
+      this.Coords.Add( v1Y_ );
+      this.Coords.Add( v1Z_ );
+      this.Faces.Add( indexPointer + 0 );
 
-      Coords.Add( ( float )v2__x );
-      Coords.Add( ( float )v2__y );
-      Coords.Add( ( float )v2__z );
+      this.Coords.Add( v2X_ );
+      this.Coords.Add( v2Y_ );
+      this.Coords.Add( v2Z_ );
+      this.Faces.Add( indexPointer + 1 );
 
-      Coords.Add( ( float )v3__x );
-      Coords.Add( ( float )v3__y );
-      Coords.Add( ( float )v3__z );
+      this.Coords.Add( v3X_ );
+      this.Coords.Add( v3Y_ );
+      this.Coords.Add( v3Z_ );
+      this.Faces.Add( indexPointer + 2 );
+
+      // Append all translated Triangle faces
+      this.Triangles.Add(
+        new NavisTriangle(
+          new NavisVertex( v1X_, v1Y_, v1Z_ ),
+          new NavisVertex( v2X_, v2Y_, v2Z_ ),
+          new NavisVertex( v3X_, v3Y_, v3Z_ )
+        )
+      );
     }
-
   }
 
   public class NavisGeometry {
@@ -81,11 +93,9 @@ namespace Rimshot.Shared {
       ModelItemCollection modelitemCollection = new ModelItemCollection {
         modelItem
       };
-      //modelitemCollection = NavisApplication.ActiveDocument.CurrentSelection.SelectedItems;
 
       //convert to COM selection
       this.ComSelection = ComBridge.ToInwOpSelection( modelitemCollection );
-
     }
     public List<CallbackGeomListener> GetFragments () {
       List<CallbackGeomListener> callbackListeners = new List<CallbackGeomListener>();
@@ -115,11 +125,9 @@ namespace Rimshot.Shared {
     public T[] ToArray<T> ( Array arr ) {
       T[] result = new T[ arr.Length ];
       Array.Copy( arr, result, result.Length );
-
       return result;
     }
   }
-
 
   public class NavisTriangle {
     public NavisVertex Vertex1 { get; set; }
