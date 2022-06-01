544970f2cdc5a369fe383e85e5a85146a1f0d3eb
Jonathon Broughton <jonathon@stardotbmp.com>
2022-06-01 18:35:27 +0100

    fix (geometry): Double and decimal accuracy to the matrix conversion

--- topics/addin/Rimshot.Shared/Geometry/Geometry.cs
+++ topics/addin/Rimshot.Shared/Geometry/Geometry.cs
@@ -9,12 +9,14 @@ namespace Rimshot.Shared {
     public List<double> Coords { get; set; }
 
     public List<int> Faces { get; set; }
-    public List<NavisTriangle> Triangles { get; set; }
-    public float[] Matrix { get; set; }
+    //public List<NavisTriangle> Triangles { get; set; }
+    public List<NavisDoubleTriangle> Triangles { get; set; }
+    public double[] Matrix { get; set; }
     public CallbackGeomListener () {
       this.Coords = new List<double>();
       this.Faces = new List<int>();
-      this.Triangles = new List<NavisTriangle>();
+      //this.Triangles = new List<NavisTriangle>();
+      this.Triangles = new List<NavisDoubleTriangle>();
     }
     public void Line ( ComApi.InwSimpleVertex v1, ComApi.InwSimpleVertex v2 ) { }
     public void Point ( ComApi.InwSimpleVertex v1 ) { }
@@ -24,38 +26,38 @@ namespace Rimshot.Shared {
       int indexPointer = this.Faces.Count;
 
       Array array_v1 = ( Array )v1.coord;
-      float v1X = ( float )array_v1.GetValue( 1 );
-      float v1Y = ( float )array_v1.GetValue( 2 );
-      float v1Z = ( float )array_v1.GetValue( 3 );
+      double v1X = ( double )( float )array_v1.GetValue( 1 );
+      double v1Y = ( double )( float )array_v1.GetValue( 2 );
+      double v1Z = ( double )( float )array_v1.GetValue( 3 );
 
       Array array_v2 = ( Array )v2.coord;
-      float v2X = ( float )array_v2.GetValue( 1 );
-      float v2Y = ( float )array_v2.GetValue( 2 );
-      float v2Z = ( float )array_v2.GetValue( 3 );
+      double v2X = ( double )( float )array_v2.GetValue( 1 );
+      double v2Y = ( double )( float )array_v2.GetValue( 2 );
+      double v2Z = ( double )( float )array_v2.GetValue( 3 );
 
       Array array_v3 = ( Array )v3.coord;
-      float v3X = ( float )array_v3.GetValue( 1 );
-      float v3Y = ( float )array_v3.GetValue( 2 );
-      float v3Z = ( float )array_v3.GetValue( 3 );
+      double v3X = ( double )( float )array_v3.GetValue( 1 );
+      double v3Y = ( double )( float )array_v3.GetValue( 2 );
+      double v3Z = ( double )( float )array_v3.GetValue( 3 );
 
       //Matrix transformation
-      float T1 = Matrix[ 3 ] * v1X + Matrix[ 7 ] * v1Y + Matrix[ 11 ] * v1Z + Matrix[ 15 ];
-      float v1X_ = ( Matrix[ 0 ] * v1X + Matrix[ 4 ] * v1Y + Matrix[ 8 ] * v1Z + Matrix[ 12 ] ) / T1;
-      float v1Y_ = ( Matrix[ 1 ] * v1X + Matrix[ 5 ] * v1Y + Matrix[ 9 ] * v1Z + Matrix[ 13 ] ) / T1;
-      float v1Z_ = ( Matrix[ 2 ] * v1X + Matrix[ 6 ] * v1Y + Matrix[ 10 ] * v1Z + Matrix[ 14 ] ) / T1;
+      double T1 = Matrix[ 3 ] * v1X + Matrix[ 7 ] * v1Y + Matrix[ 11 ] * v1Z + Matrix[ 15 ];
+      double v1X_ = ( Matrix[ 0 ] * v1X + Matrix[ 4 ] * v1Y + Matrix[ 8 ] * v1Z + Matrix[ 12 ] ) / T1;
+      double v1Y_ = ( Matrix[ 1 ] * v1X + Matrix[ 5 ] * v1Y + Matrix[ 9 ] * v1Z + Matrix[ 13 ] ) / T1;
+      double v1Z_ = ( Matrix[ 2 ] * v1X + Matrix[ 6 ] * v1Y + Matrix[ 10 ] * v1Z + Matrix[ 14 ] ) / T1;
 
-      float T2 = Matrix[ 3 ] * v2X + Matrix[ 7 ] * v2Y + Matrix[ 11 ] * v2Z + Matrix[ 15 ];
-      float v2X_ = ( Matrix[ 0 ] * v2X + Matrix[ 4 ] * v2Y + Matrix[ 8 ] * v2Z + Matrix[ 12 ] ) / T2;
-      float v2Y_ = ( Matrix[ 1 ] * v2X + Matrix[ 5 ] * v2Y + Matrix[ 9 ] * v2Z + Matrix[ 13 ] ) / T2;
-      float v2Z_ = ( Matrix[ 2 ] * v2X + Matrix[ 6 ] * v2Y + Matrix[ 10 ] * v2Z + Matrix[ 14 ] ) / T2;
+      double T2 = Matrix[ 3 ] * v2X + Matrix[ 7 ] * v2Y + Matrix[ 11 ] * v2Z + Matrix[ 15 ];
+      double v2X_ = ( Matrix[ 0 ] * v2X + Matrix[ 4 ] * v2Y + Matrix[ 8 ] * v2Z + Matrix[ 12 ] ) / T2;
+      double v2Y_ = ( Matrix[ 1 ] * v2X + Matrix[ 5 ] * v2Y + Matrix[ 9 ] * v2Z + Matrix[ 13 ] ) / T2;
+      double v2Z_ = ( Matrix[ 2 ] * v2X + Matrix[ 6 ] * v2Y + Matrix[ 10 ] * v2Z + Matrix[ 14 ] ) / T2;
 
-      float T3 = Matrix[ 3 ] * v3X + Matrix[ 7 ] * v3Y + Matrix[ 11 ] * v3Z + Matrix[ 15 ];
-      float v3X_ = ( Matrix[ 0 ] * v3X + Matrix[ 4 ] * v3Y + Matrix[ 8 ] * v3Z + Matrix[ 12 ] ) / T3;
-      float v3Y_ = ( Matrix[ 1 ] * v3X + Matrix[ 5 ] * v3Y + Matrix[ 9 ] * v3Z + Matrix[ 13 ] ) / T3;
-      float v3Z_ = ( Matrix[ 2 ] * v3X + Matrix[ 6 ] * v3Y + Matrix[ 10 ] * v3Z + Matrix[ 14 ] ) / T3;
+      double T3 = Matrix[ 3 ] * v3X + Matrix[ 7 ] * v3Y + Matrix[ 11 ] * v3Z + Matrix[ 15 ];
+      double v3X_ = ( Matrix[ 0 ] * v3X + Matrix[ 4 ] * v3Y + Matrix[ 8 ] * v3Z + Matrix[ 12 ] ) / T3;
+      double v3Y_ = ( Matrix[ 1 ] * v3X + Matrix[ 5 ] * v3Y + Matrix[ 9 ] * v3Z + Matrix[ 13 ] ) / T3;
+      double v3Z_ = ( Matrix[ 2 ] * v3X + Matrix[ 6 ] * v3Y + Matrix[ 10 ] * v3Z + Matrix[ 14 ] ) / T3;
 
 
-      this.Faces.Add( 0 ); //TRIANGLE FLAG
+      this.Faces.Add( 3 ); //TRIANGLE FLAG
 
       // Triangle by 3 Vertices
       this.Coords.Add( v1X_ );
@@ -74,13 +76,20 @@ namespace Rimshot.Shared {
       this.Faces.Add( indexPointer + 2 );
 
       // Append all translated Triangle faces
+      //this.Triangles.Add(
+      //  new NavisTriangle(
+      //    new NavisVertex( ( float )v1X_, ( float )v1Y_, ( float )v1Z_ ),
+      //    new NavisVertex( ( float )v2X_, ( float )v2Y_, ( float )v2Z_ ),
+      //    new NavisVertex( ( float )v3X_, ( float )v3Y_, ( float )v3Z_ )
+      //  )
+      //);
       this.Triangles.Add(
-        new NavisTriangle(
-          new NavisVertex( v1X_, v1Y_, v1Z_ ),
-          new NavisVertex( v2X_, v2Y_, v2Z_ ),
-          new NavisVertex( v3X_, v3Y_, v3Z_ )
-        )
-      );
+   new NavisDoubleTriangle(
+     new NavisDoubleVertex( v1X_, v1Y_, v1Z_ ),
+     new NavisDoubleVertex( v2X_, v2Y_, v2Z_ ),
+     new NavisDoubleVertex( v3X_, v3Y_, v3Z_ )
+   )
+ );
     }
   }
 
@@ -107,11 +116,12 @@ namespace Rimshot.Shared {
           ComApi.InwLTransform3f3 localToWorld = ( ComApi.InwLTransform3f3 )( object )fragment.GetLocalToWorldMatrix();
 
           //create Global Cordinate System Matrix
-          Array array_v1 = ( Array )( object )localToWorld.Matrix;
-          double[] elements = ToArray<double>( array_v1 );
-          float[] elementsValue = new float[ elements.Length ];
+          object matrix = localToWorld.Matrix;
+          Array matrix_array = ( Array )matrix;
+          double[] elements = ConvertArrayToDouble( matrix_array );
+          double[] elementsValue = new double[ elements.Length ];
           for ( int i = 0; i < elements.Length; i++ ) {
-            elementsValue[ i ] = ( float )elements[ i ];
+            elementsValue[ i ] = elements[ i ];
           }
 
           callbackListener.Matrix = elementsValue;
@@ -127,6 +137,19 @@ namespace Rimshot.Shared {
       Array.Copy( arr, result, result.Length );
       return result;
     }
+
+    public static double[] ConvertArrayToDouble ( Array arr ) {
+      if ( arr.Rank != 1 ) {
+        throw new ArgumentException();
+      }
+
+      double[] retval = new double[ arr.GetLength( 0 ) ];
+      for ( int ix = arr.GetLowerBound( 0 ); ix <= arr.GetUpperBound( 0 ); ++ix ) {
+        retval[ ix - arr.GetLowerBound( 0 ) ] = ( double )( double )arr.GetValue( ix );
+      }
+
+      return retval;
+    }
   }
 
   public class NavisTriangle {
@@ -134,6 +157,16 @@ namespace Rimshot.Shared {
     public NavisVertex Vertex2 { get; set; }
     public NavisVertex Vertex3 { get; set; }
     public NavisTriangle ( NavisVertex v1, NavisVertex v2, NavisVertex v3 ) {
+      Vertex1 = v1;
+      Vertex2 = v2;
+      Vertex3 = v3;
+    }
+  }
+  public class NavisDoubleTriangle {
+    public NavisDoubleVertex Vertex1 { get; set; }
+    public NavisDoubleVertex Vertex2 { get; set; }
+    public NavisDoubleVertex Vertex3 { get; set; }
+    public NavisDoubleTriangle ( NavisDoubleVertex v1, NavisDoubleVertex v2, NavisDoubleVertex v3 ) {
       Vertex1 = v1;
       Vertex2 = v2;
       Vertex3 = v3;
@@ -148,6 +181,16 @@ namespace Rimshot.Shared {
     public float X { get; set; }
     public float Y { get; set; }
     public float Z { get; set; }
+  }
+  public class NavisDoubleVertex {
+    public NavisDoubleVertex ( double x, double y, double z ) {
+      X = x;
+      Y = y;
+      Z = z;
+    }
+    public double X { get; set; }
+    public double Y { get; set; }
+    public double Z { get; set; }
   }
 
   public class NavisMesh {
